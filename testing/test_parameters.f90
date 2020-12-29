d2b7a7c9648ab8b4e05c70c7d5884a8b1ce311d4
Daria Turichina <t.dasha5@yandex.ru>
2020-12-29 19:33:38 +0300

    test-results

---
+++ testing/test_parameters.f90
@@ -0,0 +1,5 @@
+! Created by drakosha on 28.12.2020.
+
+program test_parameters
+
+end program test_parameters
---
+++ testing/test_q.f90
@@ -0,0 +1,5 @@
+! Created by drakosha on 29.12.2020.
+
+program test_q
+
+end program test_q
